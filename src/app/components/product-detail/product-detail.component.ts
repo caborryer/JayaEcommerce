65da248a4e62e0fc98359805f1795e0a20d0948e
caborryer <corraleshyulia@gmail.com>
2020-04-06 16:42:21 -0500

    detail products

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProductDetailComponent implements OnInit {
+  public products: {
+    id: string;
+    seller: string;
+    name: string;
+    image: string;
+    description: string;
+    units: number;
+    price: number;
+  } = {
+    id: '1',
+    name: 'Product one',
+    image: 'https://http2.mlstatic.com/D_NQ_NP_770026-MCO40460026819_012020-O.jpg',
+    seller: 'Sam',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
+    units: 2,
+    price: 22
+  };
 
   constructor() { }
 
